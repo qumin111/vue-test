da70fa05de587b280c8d0973ce5a8ee33c83d402
qumin@wecash.net <qumin@wecash.net>
2017-12-18 10:14:24 +0800

    从对象里选取特定属性的最后一个

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -25,6 +25,23 @@
             this.clientWidth = document.body.clientWidth;
             this.width = window.screen.width;
             console.log(this.$refs['box']);
+            let m = this.filterA();
+            console.log(m);
+        },
+
+        methods: {
+            filterA () {
+                let a = [{a: 'd'},{a: 'd'},{a: 's'},{a: 'b'},{a: 'd'},{a: 'm'}];
+                let needKey = ['s', 'd', 'b', 'm'];
+                for (let i in needKey) {
+                    let b = a.filter((x) => {
+                        return x.a === needKey[i];
+                    })
+                    if (b.length > 0) {
+                        return b;
+                    }
+                }
+            }   
         }
     }
 </script>
